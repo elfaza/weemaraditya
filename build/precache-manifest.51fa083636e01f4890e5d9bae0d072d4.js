3ea1fabfb24572b069dd09122bf43c46e83fff81
Mohammed Faza <Mohammed.Faza@commsult.id>
2020-06-19 21:18:10 +0700

    Add build folder

---
+++ build/precache-manifest.51fa083636e01f4890e5d9bae0d072d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99906f46e5547d04b5e6b26bc9fb5033",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3793b273825092922cd1",
+    "url": "/static/css/main.4f844cf0.chunk.css"
+  },
+  {
+    "revision": "7ee44b7b2606858a1794",
+    "url": "/static/js/2.d1b6f8e6.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.d1b6f8e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3793b273825092922cd1",
+    "url": "/static/js/main.aaf62a45.chunk.js"
+  },
+  {
+    "revision": "31feb29540a8bbeb6a94",
+    "url": "/static/js/runtime-main.aec158d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
